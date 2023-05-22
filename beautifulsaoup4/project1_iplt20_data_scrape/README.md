d4e7a970508b27dd144d2dde99a20c9519e3a2d3
Sabbir Hossain <cse.170201039@gmail.com>
2023-05-22 15:01:52 +0600

    project1 added final commit

---
+++ beautifulsaoup4/project1_iplt20_data_scrape/README.md
@@ -0,0 +1,6 @@
+# Scrape iplt20 auction unsold players data
+
+ - url = "https://www.iplt20.com/auction"
+ - all dependencies provided in requirements.txt file
+ - run pip command = pip install -r requirements.txt to install all dependencies
+ - python version 3.11.1
---
+++ beautifulsaoup4/project1_iplt20_data_scrape/main.py
@@ -0,0 +1,37 @@
+# Python scrpit for extract IPLT20 auction unsold player list with requests, bs4 and lxml parser
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from lxml import etree
+
+url = "https://www.iplt20.com/auction"
+response = requests.get(url)
+# print("response = ", response)
+
+html_content = response.content
+
+soup = BeautifulSoup(html_content, "lxml")
+
+table = soup.find("table", id="t2")
+
+# print(table)
+
+header = table.find_all('th')
+titles = []
+for th in header:
+    titles.append(th.text)
+
+df = pd.DataFrame(columns=titles)
+# print(df)
+
+rows = table.find_all("tr")
+
+for row in rows[1:]:
+    data = row.find_all("td")
+    table_row_data = [tr.text for tr in data]    
+    lenght = len(df)
+    df.loc[lenght] = table_row_data
+# print(df.head())
+
+df.to_csv("iplt20_unsold_players_2023.csv")
