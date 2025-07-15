d8bf2643b20c3ce3a4d9caf1d146c650550e670f
Md. Sabbir Hossain <sabbir.hossain@fintechhub.com.bd>
2025-07-16 01:19:18 +0600

    initial commit

--- selenium/basic_setup.py
+++ selenium/basic_setup.py
@@ -1,34 +1,59 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome("C:/Program Files (x86)/chromedriver.exe")
+service = Service(ChromeDriverManager().install())
+options = Options()
+options.add_argument('--headless')  # Enable headless mode
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(service=service, options=options)
 
 try:
-    driver.get("https://www.google.com")
+    driver.get("https://www.bing.com/")
 
     # print(driver.title)
-    time.sleep(1) # need time to load the page
+    time.sleep(3) # need time to load the page
 
-    xpath = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"
-    textarea = driver.find_element(By.XPATH, xpath) # finds goole search text input box
-    textarea.send_keys("python tutorial") # Fill the input box with text
+    input_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "sb_form_q"))) # wait until finds goole search text input box
     
-    # Click on the google search button
-    driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]").click()
-    time.sleep(2)
+    input_field.send_keys("python tutorial") # Fill the input box with text
+    print("search text written!")
+
+    time.sleep(7)
+    
+    input_field.send_keys(Keys.ENTER)
+    print("search text submitted!")
+
+    time.sleep(4)
 
     # Finds all titles of searched page
-    try:
-        titles = driver.find_elements(By.CLASS_NAME, 'LC20lb')
-        for title in titles:
-            print(title.text)
-    except:
-        print(None)
+    results = WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "b_dftt")))
+
+    titles = []
+    links = []
+
+    print("Results...")
+    for result in results:
+        titles.append(result.text)
+        links.append(result.get_attribute("href"))
+    
+    print(titles)
+    print(links)
 
     input("Insert enter to quit!....") # To close current active window press enter
-    driver.close()
     
 except:
     print(f"Website not responding! Error Occured")
+
+driver.close()
+print("Driver closed!")
